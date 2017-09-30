1230ea8cb10e1192bf66613670291f8367e7b9bc
AbhinayaRam <abhinayaramachandran@gmail.com>
2017-09-30 17:05:12 -0500

    s

--- coding-challenge-template/src/db_setup.py
+++ coding-challenge-template/src/db_setup.py
@@ -5,7 +5,7 @@ db.create_all()
 
 admin = User(username='admin')
 guest = User(username='guest')
-admin_address = Address(full_address='1 Market St, San Francisco CA 94105',
+admin_address = Address(full_address='419 Summit Ave, Arlington',
                         user=admin)
 guest_address = Address(
     full_address='1 Telegraph Hill Blvd, San Francisco, CA 94133',
